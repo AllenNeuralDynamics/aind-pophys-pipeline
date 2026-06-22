38745e3f72f5b24984c39472f2393fcacb93b547
Arielle Leon <ariellel@alleninstitute.org>
2026-06-22 10:32:21 -0700

    chore: update all container versions

--- pull_images.sh
+++ pull_images.sh
@@ -16,7 +16,7 @@ mkdir -p "$CACHE_DIR"
 
 IMAGES=(
     ghcr.io/allenneuraldynamics/motion-correction:v5
-    ghcr.io/allenneuraldynamics/ophys-movie-qc:v2
+    ghcr.io/allenneuraldynamics/ophys-movie-qc:v4
     ghcr.io/allenneuraldynamics/ophys-extraction:v4
     ghcr.io/allenneuraldynamics/ophys-roicat-classifier:v2
     ghcr.io/allenneuraldynamics/ophys-dff:v2
